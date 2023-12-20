1d763eae941bab55e38bdb32c77250dee98464f1
ManGalvan <jesus.uanl.26@outlook.com>
2023-12-20 02:47:14 -0600

    Se modifico la funcion getHTML, ahora recibe dos parametros, el primero 'package' sera la carpeta o el paquete donde se localiza y el segundo parametro 'file' es el nombre del archivo que queremos obtener. Se crearon dos nuevas funciones 'renameList' y 'addItemList' sirven para renombrar la lista actual y para agregar elementos a esa misma lista

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,12 @@
-const getHtml = (package) =>{
+const getHtml = (package, file) =>{
     let url = "";
+    if(file == ""){
+      file = "home";
+    }
     if(package != "settings"){
-        url = `packages/${package}/home_${package}.html`;
+        url = `packages/${package}/${file}_${package}.html`;
     } else {
-        url = `settings/home_${package}.html`;
+        url = `settings/${file}_${package}.html`;
     }
     fetch(url)
       .then(function(response) {
@@ -17,3 +20,41 @@ const getHtml = (package) =>{
         console.log('Hubo un error al cargar la página: ', error);
     });
 }
+
+const renameList = () => {
+  let nameList = document.getElementById("txtTitleList").value;
+  document.getElementById("txtNameList").innerText = nameList;
+};
+
+
+const addItemList = (e) => {
+  //let countElement = 0;
+  if(e.keyCode == 13){
+    let newItem = document.getElementById("txtNewItemList").value;
+
+    const li = document.createElement("li");
+    li.className = "list-group-item";
+    //li.id = `li_${countElement}`;
+
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    input.className = "form-check-input me-1";
+
+    const label = document.createElement("label");
+    label.className = "form-check-label";
+    //label.htmlFor = `li_${countElement}`;
+    label.textContent = newItem;
+
+    li.appendChild(input);
+    li.appendChild(label);
+
+    //let content = ul.appendChild(li);
+
+    document.getElementById("listing").appendChild(li);
+    document.getElementById("txtNewItemList").value = "";
+
+    //let element = liElement.append(newItem);
+    console.log(li);
+    //countElement++;
+  }
+};
